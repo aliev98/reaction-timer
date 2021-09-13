33da498171b9033ed43a526121fa7461c4876f6d
Ali Elias <kuba-98@hotmail.com>
2021-09-13 18:53:33 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Ninja reaction timer</h1>
+
+
+<button @click='start' :disabled='isPlaying' v-if='!againBtn'> {{isPlaying ? 'Playing' : 'Play'}} </button>
+<button @click='start' :disabled='isPlaying' v-if='againBtn'>  {{isPlaying ? 'Playing' : 'Play again'}} </button>
+
+<Block v-if="isPlaying" :delay="delay" @end="endGame"  />
+<Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+      againBtn: false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+      this.againBtn = true
+    }
   }
 }
 </script>
@@ -20,7 +47,25 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button{
+  background: rgb(58, 48, 194);
+  color:white;
+  padding: 5px 30px;
+  border-radius: 4px;
+  border:none;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor:pointer;
+  margin: 10px;
+}
+button:disabled{
+  opacity: 0.2;
+  cursor: not-allowed
+}
+button:hover:enabled{
+   padding: 5px 20px;
 }
 </style>
